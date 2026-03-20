eea0dbe8710d85b675b879315ef486d7b4286833
bharatmorstudies-cpu <bharatmorstudies@gmail.com>
2026-03-20 06:45:21 +0000

    Add Flask to make it as a web

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello from my Amazon EC2 instance!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello! Your Python app is now live on AWS EC2!</h1>"
+
+if __name__ == '__main__':
+    # host='0.0.0.0' makes it accessible externally
+    app.run(host='0.0.0.0', port=80) 
+
